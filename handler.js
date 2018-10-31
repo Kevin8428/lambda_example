e1bd8d5611b68635793ebfee9513f63b9483683f
Kevin Deutscher <kevin@dispatch.me>
2018-10-31 17:30:42 -0400

    second handler

--- handler.js
+++ handler.js
@@ -4,8 +4,21 @@ module.exports.hello = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
+      message: 'HELLO WORLD FUNCTION!',
+      // input: event,
+    }),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.getUser = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'GET USRE FUNCTION!',
+      // input: event,
     }),
   };
 
